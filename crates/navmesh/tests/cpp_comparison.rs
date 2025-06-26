d70a0651d2f15b5b3bd9db90d99acc802ebd85c6
Jan Hohenheim <jan@hohenheim.ch>
2025-06-26 15:41:43 +0200

    Start working on C++ tests

--- crates/navmesh/tests/cpp_comparison.rs
+++ crates/navmesh/tests/cpp_comparison.rs
@@ -6,13 +6,75 @@ use std::{
     path::{Path, PathBuf},
 };
 
+use bevy::{
+    gltf::GltfPlugin,
+    log::LogPlugin,
+    pbr::PbrPlugin,
+    prelude::*,
+    render::{mesh::MeshPlugin, view::PreviousVisibleEntities},
+    scene::SceneInstanceReady,
+};
 use serde::{Deserialize, de::DeserializeOwned};
 use serde_json::Value;
 
 #[test]
 fn initial_heightfield() {
-    let json: CppHeightfield = load_json("heightfield_initial");
-    println!("{:?}", json);
+    App::new()
+        .add_plugins((MinimalPlugins, AssetPlugin::default()))
+        .init_asset::<Shader>()
+        .init_asset::<Scene>()
+        .init_resource::<PreviousVisibleEntities>()
+        .add_plugins((
+            GltfPlugin::default(),
+            MeshPlugin,
+            PbrPlugin::default(),
+            LogPlugin::default(),
+        ))
+        .add_systems(Startup, load_heightfield)
+        .add_systems(Update, handle_asset_event)
+        .add_observer(compare_heightfields)
+        .run();
+}
+
+fn load_heightfield(asset_server: Res<AssetServer>, mut commands: Commands) {
+    let virtual_path = "models/dungeon.gltf#Mesh0/Primitive0";
+    let global_path = virtual_path_to_global_path(virtual_path);
+    if !global_path.exists() {
+        panic!("Asset not found: {global_path:?}");
+    }
+
+    let mesh = asset_server.load(virtual_path);
+    println!("{:?}", asset_server.get_load_state(mesh.id()));
+    commands.spawn(Mesh3d(mesh));
+}
+
+#[derive(Event)]
+struct MeshLoaded(AssetId<Mesh>);
+
+fn handle_asset_event(mut events: EventReader<AssetEvent<Mesh>>, mut commands: Commands) {
+    for event in events.read() {
+        match event {
+            AssetEvent::LoadedWithDependencies { id } => {
+                commands.trigger(MeshLoaded(*id));
+            }
+            AssetEvent::Removed { id } => {
+                panic!("Failed to load asset {id:?}");
+            }
+            AssetEvent::Unused { id } => {
+                panic!("Failed to load asset {id:?}");
+            }
+            AssetEvent::Added { id } => {
+                info!("Added: {id:?}");
+            }
+            AssetEvent::Modified { id } => {
+                info!("Modified: {id:?}");
+            }
+        }
+    }
+}
+
+fn compare_heightfields(trigger: Trigger<MeshLoaded>, meshes: Res<Assets<Mesh>>) {
+    let mesh = meshes.get(trigger.event().0).unwrap();
 }
 
 #[derive(Debug, Deserialize)]
@@ -46,7 +108,6 @@ pub enum EmptyOption<T> {
 fn load_json<T: DeserializeOwned>(name: &str) -> T {
     let test_path = env::current_dir()
         .unwrap()
-        .join("tests")
         .join("reference_data")
         .join(format!("{name}.json"));
 
@@ -59,4 +120,13 @@ fn load_json<T: DeserializeOwned>(name: &str) -> T {
     serde_json::from_value(value).unwrap_or_else(|e| {
         panic!("Failed to deserialize JSON: {}: {}", test_path.display(), e);
     })
+}
+
+fn virtual_path_to_global_path(virtual_path: &str) -> PathBuf {
+    // remove everything after the first #
+    let stripped = virtual_path.split('#').next().unwrap();
+    let mut path = env::current_dir().unwrap();
+    path.push("assets");
+    path.push(stripped);
+    path
 }
