5261abecaf4593826c47f8afa51362a6a45e87e5
Jan Hohenheim <jan@hohenheim.ch>
2025-08-05 07:49:14 +0200

    Update nav test

--- crates/bevy_rerecast/tests/regression.rs
+++ crates/bevy_rerecast/tests/regression.rs
@@ -14,7 +14,7 @@ use bevy_rerecast::{Mesh3dBackendPlugin, debug::NavmeshDebugPlugin, prelude::*};
 use bevy_rerecast_editor_integration::NavmeshEditorIntegrationPlugin;
 
 #[test]
-fn validate_bevy_navmesh_against_cpp_implementation() {
+fn test_gltf_generation() {
     let mut app = App::new();
     app.add_plugins(headless_plugins);
 
